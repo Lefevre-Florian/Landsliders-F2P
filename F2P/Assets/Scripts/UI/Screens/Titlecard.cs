bd627888ab5dee8b7fc2bbcc04a6c74986768592
Lefevre-Florian <lefevreflorian18@gmail.com>
2024-04-02 16:22:08 +0200

    feat(UI): loading screen + improve titlecard + titlecard anchors

--- F2P/Assets/Scripts/UI/Screens/Titlecard.cs
+++ F2P/Assets/Scripts/UI/Screens/Titlecard.cs
@@ -1,8 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
 // Author (CR) : Lefevre Florian
 namespace Com.IsartDigital.F2P.UI.Screens
 {
     public class Titlecard : Screen
     {
-        public void Play() { }
+        [Header("Scene management")]
+        [SerializeField] private bool _IsUsingLoadingScreen = false;
+        [SerializeField] private int _GameBuildIDX = 0;
+
+        [Header("Application management")]
+        [SerializeField] private int _TargetedFrameRate = 30;
+
+        private void Start()
+        {
+            Application.targetFrameRate = _TargetedFrameRate;
+
+            // Reset timescale (in case)
+            Time.timeScale = 1f;
+        }
+
+        public void Play()
+        {
+            if (!_IsUsingLoadingScreen)
+                SceneManager.LoadScene(_GameBuildIDX);
+            else
+                LoadManager.GetInstance().StartLoading(_GameBuildIDX);
+        }
     }
 }
