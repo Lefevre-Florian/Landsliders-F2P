a5991a497900e4a157fb063c89132055b7b2eadb
Paul <98259426+Choooca@users.noreply.github.com>
2024-04-09 15:33:39 +0200

    Revert "Add Scriptable Card and Component Serialization"
    This reverts commit 87b5a55d224309384effe23408ac6b4a4428d226.

--- F2P/Assets/Scripts/Gameplay/Card/CardContainer.cs
+++ F2P/Assets/Scripts/Gameplay/Card/CardContainer.cs
@@ -1,40 +1,16 @@
-using System;
+using System.Collections;
 using System.Collections.Generic;
-using TMPro;
-using Unity.VisualScripting;
-using UnityEditor;
 using UnityEngine;
 
 namespace com.isartdigital.f2p.gameplay.card 
 {
-    public enum CardTypes {
-        NONE,
-        PLAINE
-    }
-
     [RequireComponent(typeof(BoxCollider2D))]
     public class CardContainer : MonoBehaviour
     {
         [HideInInspector] public BoxCollider2D boxCollider;
 
-        [SerializeField] private List<ScriptableCard> allCardsParameters = new List<ScriptableCard>();
-
-        private ScriptableCard myCard;
-
         [HideInInspector] public float size;
         [HideInInspector] public float cardRatio = 1.39f;
-
-        private void Start()
-        {
-
-        }
-
-        public void Init() 
-        {
-            myCard = allCardsParameters[Mathf.FloorToInt(UnityEngine.Random.Range(0, allCardsParameters.Count))];
-            foreach (MonoScript script in myCard.Scripts)
-                gameObject.AddComponent(script.GetClass());
-        }
 
         public BoxCollider2D GetBoxCollider() 
         {
