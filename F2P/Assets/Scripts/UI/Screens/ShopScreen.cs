be6a2d4020d1a7075d3396ab5f0123d239338fb8
DRIDI Elias <e.dridi@student.isartdigital.com>
2024-04-11 11:00:58 +0200

    [ADD + MODIFY] IAP System + Shop UI

---
+++ F2P/Assets/Scripts/UI/Screens/ShopScreen.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Com.IsartDigital.F2P.UI
+{
+    public class ShopScreen : Screen
+    {
+        
+    }
+}
+
