aec0bf357e38986b535fe7a3323f59d34ab337bc
Lefevre-Florian <lefevreflorian18@gmail.com>
2024-05-15 16:41:52 +0200

    [ADD] Splashscreen

---
+++ F2P/Assets/Scripts/Managers/PreLauncher.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+
+namespace Com.IsartDigital.F2P
+{
+    public class PreLauncher : MonoBehaviour
+    {
+  
+        void Start()
+        {
+            
+        }
+
+    }
+}
