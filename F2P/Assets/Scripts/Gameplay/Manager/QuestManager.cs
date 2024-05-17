7cfb5e192bddd224b7a89496230b549fb63a000d
Paul <98259426+Choooca@users.noreply.github.com>
2024-05-17 16:11:34 +0200

    [ADD] Quest label dic

--- F2P/Assets/Scripts/Gameplay/Manager/QuestManager.cs
+++ F2P/Assets/Scripts/Gameplay/Manager/QuestManager.cs
@@ -1,4 +1,6 @@
+using Com.IsartDigital.F2P.Biomes;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 using UnityEngine;
@@ -15,6 +17,7 @@ namespace com.isartdigital.f2p.manager
 
         public static UnityEvent ValidQuest = new UnityEvent();
 
+        [SerializeField] private QuestLabelsDic _QuestLabelsDic = new QuestLabelsDic();
         public enum QuestsEnum
         {
             NONE,
@@ -62,6 +65,41 @@ namespace com.isartdigital.f2p.manager
             GameFlowManager.LoadMap.RemoveListener(Init);
             ValidQuest.RemoveListener(WinDebug);
         }
+    }
+
+    [Serializable]
+    public class QuestLabelsDic
+    {
+        [SerializeField] QuestLabelItem[] _Dict;
+
+        public Dictionary<QuestManager.QuestsEnum, QuestText> ToDic()
+        {
+            Dictionary<QuestManager.QuestsEnum, QuestText> newDic = new Dictionary<QuestManager.QuestsEnum, QuestText>();
+
+            foreach (QuestLabelItem item in _Dict)
+            {
+                newDic.Add(item.key, item.value);
+            }
+
+            return newDic;
+        }
+    }
+
+    [Serializable]
+    public class QuestLabelItem
+    {
+        [SerializeField]
+        public QuestManager.QuestsEnum key;
+
+        [SerializeField]
+        public QuestText value;
+    }
+
+    [Serializable]
+    public struct QuestText
+    {
+        public string name;
+        public string desc;
     }
 }
 
