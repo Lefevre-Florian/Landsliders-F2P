6124e951e5b6ccbe4ae6674386a0c39647e6defe
Lefevre-Florian <lefevreflorian18@gmail.com>
2024-05-23 01:07:56 +0200

    [MODIFY] Refactoring dialogue system
    Again

--- F2P/Assets/Scripts/FTUE/Dialogues/DialogueLinePrinting.cs
+++ F2P/Assets/Scripts/FTUE/Dialogues/DialogueLinePrinting.cs
@@ -1,19 +1,36 @@
+using System;
 using System.Collections;
 
 using TMPro;
 
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 // Author (CR) : Lefevre Florian
 namespace Com.IsartDigital.F2P.FTUE.Dialogues
 {
     public class DialogueLinePrinting : DialogueScreen
     {
+        [Serializable]
+        public struct StoryPage
+        {
+            public Sprite image;
+            public string id;
+        }
+
         [SerializeField] private TextMeshProUGUI _DisplayContinue = null;
+        [SerializeField] private Image _DisplayImage = null;
+
+        [Header("Flow")]
+        [SerializeField] private StoryPage[] _Pages = new StoryPage[0];
+
+        [Space(2)]
+        [SerializeField][Range(.01f, 1f)] private float _TimeBeforeSwitchingPage = 0.5f;
 
         // Variables
         private int _CurrentLineID = 0;
+        private string _CurrentLine = "";
 
         // Events
         [HideInInspector]
@@ -22,10 +39,13 @@ namespace Com.IsartDigital.F2P.FTUE.Dialogues
         protected override void Start()
         {
             base.Start();
+            int lLength = _Pages.Length;
+            m_DialogueIDs = new string[lLength];
+            for (int i = 0; i < lLength; i++)
+                m_DialogueIDs[i] = _Pages[i].id;
+
             if (m_DialogueIDs.Length > 0)
-            {
                 DisplayText();
-            }
 
             _DisplayContinue.gameObject.SetActive(false);
         }
@@ -37,10 +57,15 @@ namespace Com.IsartDigital.F2P.FTUE.Dialogues
             int lLength = m_DialogueIDs.Length;
             while (_CurrentLineID <= m_DialogueIDs.Length - 1)
             {
-                yield return WriteLine(_CurrentLineID);
+                m_LabelUIText.text = "";
 
+                _DisplayImage.sprite = _Pages[_CurrentLineID].image;
+                _CurrentLine = m_DialogueManager.GetDialogue(_Pages[_CurrentLineID].id);
+
+                yield return WriteLine();
+                yield return new WaitForSeconds(_TimeBeforeSwitchingPage);
+                
                 _CurrentLineID += 1;
-                m_LabelUIText.text += '\n';
             }
 
             StopCoroutine(m_DialogueWriter);
@@ -51,39 +76,31 @@ namespace Com.IsartDigital.F2P.FTUE.Dialogues
             m_DialogueManager.OnScreenTouched += WaitForContinue;
         }
 
-        private IEnumerator WriteLine(int pID)
+        private IEnumerator WriteLine()
         {
-            string lLine = m_DialogueManager.GetDialogue(m_DialogueIDs[pID]);
-
-            int lLength = lLine.Length;
+            int lLength = _CurrentLine.Length;
             float t = m_DisplayDuration / lLength;
             for (int i = 0; i < lLength; i++)
             {
                 yield return new WaitForSeconds(t);
-                m_LabelUIText.text += lLine[i];
+                m_LabelUIText.text += _CurrentLine[i];
             }
         }
 
         private void SkipTextPrinting()
         {
-            if(m_DialogueWriter != null)
-                StopCoroutine(m_DialogueWriter);
-            
-            string lLines = "";
-            int lLength = m_DialogueIDs.Length;
-
-            for (int i = 0; i < lLength; i++)
-            {
-                lLines += m_DialogueManager.GetDialogue(m_DialogueIDs[i]);
-                lLines += '\n';
-            }
-            m_LabelUIText.text = lLines;
+            StopCoroutine(WriteLine());
 
             // End dialogue printing
-            _DisplayContinue.gameObject.SetActive(true);
+            if (_CurrentLineID == _Pages.Length)
+            {
+                StopAllCoroutines();
 
-            m_DialogueManager.OnScreenTouched -= SkipTextPrinting;
-            m_DialogueManager.OnScreenTouched += WaitForContinue;
+                _DisplayContinue.gameObject.SetActive(true);
+
+                m_DialogueManager.OnScreenTouched -= SkipTextPrinting;
+                m_DialogueManager.OnScreenTouched += WaitForContinue;
+            }
         }
 
         private void WaitForContinue()
