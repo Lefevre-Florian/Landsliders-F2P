e156a8966b2d847224e958388ce5aee503fa168b
Lefevre-Florian <lefevreflorian18@gmail.com>
2024-05-22 22:33:49 +0200

    [MODIFY] Add new card renderer to deck screen

--- F2P/Assets/Scripts/UI/Screens/UpgradeScreen.cs
+++ F2P/Assets/Scripts/UI/Screens/UpgradeScreen.cs
@@ -1,4 +1,3 @@
-using Com.IsartDigital.F2P.Cards;
 using TMPro;
 
 using UnityEngine;
@@ -13,47 +12,10 @@ namespace Com.IsartDigital.F2P.UI.Screens
         [SerializeField] private TextMeshProUGUI _DescrLabel = null;
         [SerializeField] private RawImage _Image = null;
 
-        // Variables
-        private RenderTexture _VirtualTexture = null;
-
-        public override void Open()
-        {
-            Vector2 lSize = _Image.GetComponent<RectTransform>().rect.size;
-            TexturePhotographer lPhotographer = TexturePhotographer.GetInstance();
-
-            _VirtualTexture = lPhotographer.CreateEmptyTexture(lSize);
-            _VirtualTexture.Create();
-
-            base.Open();
-        }
-
-        public void SetContent(string pDescription, GameObject pModel)
+        public void SetContent(string pDescription, Texture2D pTexture2D)
         {
             _DescrLabel.text = pDescription;
-            _Image.texture = _VirtualTexture;
-
-            GameObject lObj = TexturePhotographer.GetInstance().StartRecording(_VirtualTexture, pModel, new Vector2(.45f, .45f));
-            if (lObj != null)
-            {
-                CardRenderer lCard = lObj.GetComponent<CardRenderer>();
-                lCard.EnableAnimation();
-                lCard.FlipCard();
-            }
-                
-        }
-
-        public override void Close()
-        {
-            
-            if(_VirtualTexture != null)
-            {
-                TexturePhotographer.GetInstance()
-                                   .StopRecording();
-
-                _VirtualTexture.Release();
-                _VirtualTexture = null;
-            }
-            base.Close();
+            _Image.texture = pTexture2D;
         }
     }
 }
