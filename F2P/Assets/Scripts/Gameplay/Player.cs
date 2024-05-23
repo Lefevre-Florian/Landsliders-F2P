4eaad12c7f57ad719fbe2b47bd9bba7747a20b7d
Lefevre-Florian <lefevreflorian18@gmail.com>
2024-05-23 15:11:15 +0200

    [MODIFY] Change deck screen to add description

--- F2P/Assets/Scripts/Gameplay/Player.cs
+++ F2P/Assets/Scripts/Gameplay/Player.cs
@@ -298,7 +298,7 @@ public class Player : MonoBehaviour
 
         }
 
-        //GameManager.GetInstance().SetModeGameover();
+        GameManager.GetInstance().SetModeGameover();
     }
 
     private void OnDestroy()
